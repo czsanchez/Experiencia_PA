57ba174b65b9d9f69a73ede5459ce756c0252d5a
czsanchez <127255444+czsanchez@users.noreply.github.com>
2024-03-13 01:11:42 +0000

    Experiencia :D

--- README.md
+++ README.md
@@ -1,41 +1,6 @@
-# PA
+# PA - Mimi
+## Recuerda que copiar está prohibido e infringe las reglas del curso.
+### Además los repos dejan ver los watchers a la derecha. Cuidado. :eyes:
 
-Guia experiencia Programación avanzada
+<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Far.pinterest.com%2Fpin%2F646055509063171699%2F&psig=AOvVaw2OpfUIgcav__pV39fItQxF&ust=1710378279749000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIiXieGF8IQDFQAAAAAdAAAAABAn"/>
 
-## Pasos (Paso 1 y 2 los hace solo una persona del grupo)
-
-### 1: Clona este repositorio en tu cuenta personal
-
-1. Inicia sesión en Github
-2. Apreta el botón fork
-![Fork repository](img/fork.png)
-
-
-### 2: Invita a tu compañero/a a colaborar
-
-1. Una vez clonado el repositorio, anda a Settings
-![Go to settings](img/settings.png)
-2. Selecciona "collaborators"
-![Go to collaborators](img/collaborators.png)
-3. Clickea en "add people"
-![Add people](img/add_people.png)
-4. Busca el nombre de tu compañero/a en github e invitalo a colaborar
-
-### 3. Creación de Codespaces
-
-1. Vuelve a la base de tu repositorio
-![Go to code](img/code.png)
-2. Selecciona "<> Code"
-![Go to green code](img/green_code.png)
-3. Clickea en "Create codespace on main"
-![Create codespace](img/create_codespaces.png)
-
-### 4: Instala python en el entorno
-
-1. Apreta el boton de extensiones de la barra lateral y busca python
-2. Haz click en instalar
-    ![alt text](img/installing_python.png)
-
-### 5. Soluciona los desafios y guarda los cambios
-
-1. Cada integrante debe solucionar uno de los desafios. Una vez que hayas hecho el desafío, tienes que subirlo al repositorio (recuerda los comandos vistos en la clase)
--- juegos.py
+++ juegos.py
@@ -1,5 +1,6 @@
 import random
 import time
+import datetime
 
 def cachipun():
     """
@@ -7,7 +8,27 @@ def cachipun():
     Debes pedir al usuario que elija piedra, papel o tijera, y luego comparar su elección con la de la computadora.
     La computadora debe elegir una opción al azar.
     """
+    eleccion= int(input("Escoge piedra (0), papel (1) o tijera (2): "))
+    while eleccion not in [0,1,2]:
+        eleccion= int(input("Selección inválida. Por favor vuelve a escoger.\nEscoge piedra (0), papel (1) o tijera (2): "))
+    compu= random.randint(0,2)
+    
+    if compu==0:
+        compustr="piedra."
+    elif compu==1:
+        compustr="papel."
+    else:
+        compustr="tijera."
+
+    if eleccion == compu:
+        print(f"¡Empate! El computador escogió {compustr}")
+    elif (eleccion==0 and compu==2) or (eleccion==1 and compu==0) or (eleccion==2 and compu==1):
+        print(f"¡Ganaste! El computador escogió {compustr}")
+    else:
+        print(f"Perdiste. El computador escogió {compustr} :(")
+
     pass
+    
 
 def adivinar_numero():
     """
@@ -15,6 +36,13 @@ def adivinar_numero():
     Debes generar un número al azar entre 1 y 10, y luego pedir al usuario que adivine el número.
     Se debe mostrar un mensaje si el usuario adivina correctamente o no.
     """
+    num_azar= random.randint(1,10)
+    player= int(input("Estoy generando un número del 1-10. Adivina cuál es: "))
+
+    if num_azar==player:
+        print(f"Ganaste. El número era {num_azar}.")
+    else:
+        print(f"Perdiste. El número era {num_azar}. :(")
     pass
 
 def reaccion_rapida():
@@ -23,6 +51,15 @@ def reaccion_rapida():
     Debes pedir al usuario que presione Enter lo más rápido posible después de que vea "GO!".
     Debes medir el tiempo que tarda el usuario en reaccionar y mostrarlo al final del juego.
     """
+    time.sleep(0.5)
+    print("Este es un juego de reacción. En cualquiera de los siguientes segundos tendrás que presionar enter..")
+    time.sleep(random.randint(1,12))
+    inicio = time.time()
+    t = input("GO!")
+    press = time.time()
+    dif = press-inicio
+    print(f"Tu tiempo de reacción fue de {dif} segundos.")
+
     pass
 
 def suma_rapida():
@@ -31,6 +68,19 @@ def suma_rapida():
     Debes generar dos números al azar y pedir al usuario que los sume lo más rápido posible.
     Debes medir el tiempo que tarda el usuario en responder y mostrarlo al final del juego.
     """
+    time.sleep(0.5)
+    print("¡Suma lo más rápido posible!")
+    time.sleep(1)
+    num1= random.randint(1,20)
+    num2=random.randint(1,20)
+    inicio = time.time()
+    t = int(input(f"¿{num1} + {num2}? "))
+    press = time.time()
+    dif = press-inicio
+    if num1+num2==t:
+        print(f"¡Correcto, felicidades! Tu tiempo de reacción fue de {dif} segundos.")
+    else:
+        print(f"Incorrecto. Tu tiempo de reacción fue de {dif} segundos. :(")
     pass
 
 def memoria():
@@ -40,4 +90,24 @@ def memoria():
     Luego, debes pedir al usuario que repita la secuencia.
     Se debe mostrar un mensaje si el usuario acierta o no.
     """
+    print("Te voy a mostrar 10 números. Memorízalos: ")
+    listanums=[]
+    for i in range(10):
+        num=random.randint(0,9)
+        listanums.append(num)
+        print(num)
+        time.sleep(1)
+    for i in range(10):
+        print("-"*20)
+    num_adiv=input("¿Cuál era la secuencia? " )
+    num_adiv=num_adiv.split()
+    contador=0
+    for i in range(10):
+        if listanums[i]==int(num_adiv[i]):
+            contador+=1
+    if contador==10:
+        print("¡Correcto! Esa era la secuencia. :)")
+    else:
+        print(f"Te equivocaste. Secuencia incorrecta, tuviste {contador} aciertos. :(")
+
     pass
